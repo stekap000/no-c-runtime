9b58b6ee39c5e57537bb8eeedc19d4bebbc6b746
stekap000 <stekap000@gmail.com>
2025-08-20 22:01:17 +0200

    _fltused and _allrem

---
+++ win_custom_crt_float.cpp
@@ -0,0 +1,3 @@
+extern "C" {
+    int _fltused;
+}
--- win_custom_crt_math.cpp
+++ win_custom_crt_math.cpp
@@ -1,7 +1,7 @@
 #ifdef _M_IX86
 
 // Implementation for symbols which are called by MSVC when 64-bit math operations are used on x86.
-// Symbols:
+// Symbols (one underscore, since the versions with two underscores are mangled names):
 //     _alldiv
 //     _allmul
 //     _allrem
@@ -218,7 +218,168 @@ hard:
     }
 
     __declspec(naked) void _allrem() {
+        #define CUSTOM_CRT_DVND esp + 12
+        #define CUSTOM_CRT_DVSR esp + 20
 
+        __asm {
+            push    ebx
+            push    edi
+
+            // Set up the local stack and save the index registers.  When this is done
+            // the stack frame will look as follows (assuming that the expression a%b will
+            // generate a call to lrem(a, b)):
+            //
+            //               -----------------
+            //               |               |
+            //               |---------------|
+            //               |               |
+            //               |--divisor (b)--|
+            //               |               |
+            //               |---------------|
+            //               |               |
+            //               |--dividend (a)-|
+            //               |               |
+            //               |---------------|
+            //               | return addr** |
+            //               |---------------|
+            //               |       EBX     |
+            //               |---------------|
+            //       ESP---->|       EDI     |
+            //               -----------------
+            //
+
+            // Determine sign of the result (edi = 0 if result is positive, non-zero
+            // otherwise) and make operands positive.
+
+            xor     edi,edi         // result sign assumed positive
+
+            mov     eax,CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVND) // hi word of a
+            or      eax,eax         // test to see if signed
+            jge     short L1        // skip rest if a is already positive
+            inc     edi             // complement result sign flag bit
+            mov     edx,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVND) // lo word of a
+            neg     eax             // make a positive
+            neg     edx
+            sbb     eax,0
+            mov     CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVND),eax // save positive value
+            mov     CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVND),edx
+L1:
+            mov     eax,CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVSR) // hi word of b
+            or      eax,eax         // test to see if signed
+            jge     short L2        // skip rest if b is already positive
+            mov     edx,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVSR) // lo word of b
+            neg     eax             // make b positive
+            neg     edx
+            sbb     eax,0
+            mov     CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVSR),eax // save positive value
+            mov     CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVSR),edx
+L2:
+
+            // Now do the divide.  First look to see if the divisor is less than 4194304K.
+            // If so, then we can use a simple algorithm with word divides, otherwise
+            // things get a little more complex.
+            //
+            // NOTE - eax currently contains the high order word of CUSTOM_CRT_DVSR
+
+            or      eax,eax         // check to see if divisor < 4194304K
+            jnz     short L3        // nope, gotta do this the hard way
+            mov     ecx,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVSR) // load divisor
+            mov     eax,CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVND) // load high word of dividend
+            xor     edx,edx
+            div     ecx             // edx <- remainder
+            mov     eax,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVND) // edx:eax <- remainder:lo word of dividend
+            div     ecx             // edx <- final remainder
+            mov     eax,edx         // edx:eax <- remainder
+            xor     edx,edx
+            dec     edi             // check result sign flag
+            jns     short L4        // negate result, restore stack and return
+            jmp     short L8        // result sign ok, restore stack and return
+
+            //
+            // Here we do it the hard way.  Remember, eax contains the high word of CUSTOM_CRT_DVSR
+            //
+
+L3:
+            mov     ebx,eax         // ebx:ecx <- divisor
+            mov     ecx,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVSR)
+            mov     edx,CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVND) // edx:eax <- dividend
+            mov     eax,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVND)
+L5:
+            shr     ebx,1           // shift divisor right one bit
+            rcr     ecx,1
+            shr     edx,1           // shift dividend right one bit
+            rcr     eax,1
+            or      ebx,ebx
+            jnz     short L5        // loop until divisor < 4194304K
+            div     ecx             // now divide, ignore remainder
+
+            //
+            // We may be off by one, so to check, we will multiply the quotient
+            // by the divisor and check the result against the original dividend
+            // Note that we must also check for overflow, which can occur if the
+            // dividend is close to 2**64 and the quotient is off by 1.
+            //
+
+            mov     ecx,eax         // save a copy of quotient in ECX
+            mul     dword ptr CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVSR)
+            xchg    ecx,eax         // save product, get quotient in EAX
+            mul     dword ptr CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVSR)
+            add     edx,ecx         // EDX:EAX = QUOT * CUSTOM_CRT_DVSR
+            jc      short L6        // carry means Quotient is off by 1
+
+            //
+            // do long compare here between original dividend and the result of the
+            // multiply in edx:eax.  If original is larger or equal, we are ok, otherwise
+            // subtract the original divisor from the result.
+            //
+
+            cmp     edx,CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVND) // compare hi words of result and original
+            ja      short L6        // if result > original, do subtract
+            jb      short L7        // if result < original, we are ok
+            cmp     eax,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVND) // hi words are equal, compare lo words
+            jbe     short L7        // if less or equal we are ok, else subtract
+L6:
+            sub     eax,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVSR) // subtract divisor from result
+            sbb     edx,CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVSR)
+L7:
+
+            //
+            // Calculate remainder by subtracting the result from the original dividend.
+            // Since the result is already in a register, we will do the subtract in the
+            // opposite direction and negate the result if necessary.
+            //
+
+            sub     eax,CUSTOM_CRT_LOWORD(CUSTOM_CRT_DVND) // subtract dividend from result
+            sbb     edx,CUSTOM_CRT_HIWORD(CUSTOM_CRT_DVND)
+
+            //
+            // Now check the result sign flag to see if the result is supposed to be positive
+            // or negative.  It is currently negated (because we subtracted in the 'wrong'
+            // direction), so if the sign flag is set we are done, otherwise we must negate
+            // the result to make it positive again.
+            //
+
+            dec     edi             // check result sign flag
+            jns     short L8        // result is ok, restore stack and return
+L4:
+            neg     edx             // otherwise, negate the result
+            neg     eax
+            sbb     edx,0
+
+            //
+            // Just the cleanup left to do.  edx:eax contains the quotient.
+            // Restore the saved registers and return.
+            //
+
+L8:
+            pop     edi
+            pop     ebx
+
+            ret     16
+        }
+
+        #undef CUSTOM_CRT_DVND
+        #undef CUSTOM_CRT_DVSR
     }
 
     __declspec(naked) void _allshl() {
