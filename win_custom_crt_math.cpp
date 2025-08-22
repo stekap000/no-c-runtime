0634b58379e9d51e550353f38c7dfecf15e710df
stekap000 <stekap000@gmail.com>
2025-08-22 21:24:24 +0200

    _allshr

--- win_custom_crt_math.cpp
+++ win_custom_crt_math.cpp
@@ -392,7 +392,6 @@ extern "C" {
 
     __declspec(naked) void _allshl() {
         __asm {
-            // ***
             // llshl - long shift left
             //
             // Purpose:
@@ -411,7 +410,6 @@ extern "C" {
             //
             // Exceptions:
             //
-            // *******************************************************************************
 
             //  Handle shifts of 64 or more bits (all get 0)
             cmp     cl, 64
@@ -441,7 +439,53 @@ extern "C" {
     }
 
     __declspec(naked) void _allshr() {
+        __asm {
+            // llshr - long shift right
+            //
+            // Purpose:
+            //        Does a signed Long Shift Right
+            //        Shifts a long right any number of bits.
+            //
+            // Entry:
+            //        EDX:EAX - long value to be shifted
+            //        CL    - number of bits to shift by
+            //
+            // Exit:
+            //        EDX:EAX - shifted value
+            //
+            // Uses:
+            //        CL is destroyed.
+            //
+            // Exceptions:
+            //
 
+
+            //  Handle shifts of 64 bits or more (if shifting 64 bits or more, the result
+            //  depends only on the high order bit of edx).
+            cmp     cl,64
+            jae     short RETSIGN
+
+            //  Handle shifts of between 0 and 31 bits
+            cmp     cl, 32
+            jae     short MORE32
+            shrd    eax,edx,cl
+            sar     edx,cl
+            ret
+
+            //  Handle shifts of between 32 and 63 bits
+            MORE32:
+            mov     eax,edx
+            sar     edx,31
+            and     cl,31
+            sar     eax,cl
+            ret
+
+            //  Return double precision 0 or -1, depending on the sign of edx
+            RETSIGN:
+            sar     edx,31
+            mov     eax,edx
+            ret
+        }
     }
 
     __declspec(naked) void _aulldiv() {
