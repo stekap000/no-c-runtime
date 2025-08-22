af4d57ca0c545b8e5062968ff79275dea09826f5
stekap000 <stekap000@gmail.com>
2025-08-22 23:41:14 +0200

    simple string print

--- main.cpp
+++ main.cpp
@@ -1,21 +1,43 @@
 #include <windows.h>
 #include <stdint.h>
 
-// before: 105472
-// after : 2560
+#include "win_custom_crt_math.cpp"
+#include "win_custom_crt_float.cpp"
 
-void __stdcall WinMainCRTStartup() {
-    int result = WinMain(GetModuleHandle(0), 0, 0, 0);
-    ExitProcess(result);
+typedef uint8_t   u8;
+typedef int8_t    s8;
+typedef uint16_t  u16;
+typedef int16_t   s16;
+typedef uint32_t  u32;
+typedef int32_t   s32;
+typedef uint64_t  u64;
+typedef int64_t   s64;
+typedef float     f32;
+typedef double    f64;
+
+HANDLE std_out;
+
+struct String {
+    char* data;
+    u64 size;
+    String(char* data, u64 size) : data(data), size(size) {}
+};
+#define StringLiteral(s) String((char*)s, sizeof(s))
+
+void print(String s) {
+    DWORD count;
+    WriteConsole(std_out, s.data, s.size, &count, NULL);
 }
 
-int CALLBACK
-WinMain(HINSTANCE Instance,
-        HINSTANCE PrevInstance,
-        LPSTR CommandLine,
-        int ShowCode)
-{
+void __stdcall WinMainCRTStartup() {
+    AttachConsole(ATTACH_PARENT_PROCESS);
+    std_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (std_out == NULL || std_out == INVALID_HANDLE_VALUE)
+    {
+        ExitProcess(ERROR_INVALID_HANDLE);
+    }
 
+    print(StringLiteral("Printing for debug works"));
 
-    return 0;
+    ExitProcess(0);
 }
