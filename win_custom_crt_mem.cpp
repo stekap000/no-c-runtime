f672c0f6d16842d17eaa3f527af3c612de90d0bd
stekap000 <stekap000@gmail.com>
2025-08-23 21:33:31 +0200

    basic memset and memcpy functions for compiler (without intrinsics)

---
+++ win_custom_crt_mem.cpp
@@ -0,0 +1,24 @@
+extern "C" {
+    #pragma function(memset)
+    void* memset(void* destination, int byte_value, size_t byte_count) {
+        char* dest = (char*)destination;
+
+        while(byte_count--) {
+            *dest++ = (char)byte_value;
+        }
+
+        return destination;
+    }
+
+    #pragma function(memcpy)
+    void* memcpy(void *destination, const void *source, size_t byte_count) {
+        char* dest = (char*)destination;
+        char* src = (char*)source;
+
+        while(byte_count--) {
+            *dest++ = *src++;
+        }
+
+        return destination;
+    }
+}
